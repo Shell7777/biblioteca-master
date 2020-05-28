f5f9043dd84c8e67aa2f335491f5aadc5fe7f13e
Elvis <asdasd@asdasd.cm>
2020-05-27 23:55:10 -0500

    Otro commit

---
+++ BibliotecaUPN.Test/Selenium/Selenium.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaUPN.Test.Selenium
+{
+    class Selenium
+    {
+
+        IWebDriver chrome = new ChromeDriver();
+
+       // chrome.Url = "https://duckduckgo.com/";
+
+        //Me falto tiempo y me salio error. Perdon, Profesor :C no lo hice 
+      
+
+    }
+}
